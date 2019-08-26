9aa728e9b74403da5ac52e14d9781241a07ef003
KenYeh1023 <s1011451@gmail.com>
2019-08-26 18:50:06 +0800

    新增揪團頁面UI/NavigationViewControllwe/TabBarViewController

---
+++ Alien/GroupListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GroupListTableViewCell.swift
+//  Alien
+//
+//  Created by Ken Yeh on 2019/8/26.
+//  Copyright © 2019 Ken Yeh. All rights reserved.
+//
+
+import UIKit
+
+class GroupListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
