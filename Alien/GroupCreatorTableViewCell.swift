6455189e5b36d4c84c4297dce990e184c9277b7c
KenYeh1023 <s1011451@gmail.com>
2019-08-28 17:20:18 +0800

    新增遊戲選擇按鈕

---
+++ Alien/GroupCreatorTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GroupCreatorTableViewCell.swift
+//  Alien
+//
+//  Created by Ken Yeh on 2019/8/28.
+//  Copyright © 2019 Ken Yeh. All rights reserved.
+//
+
+import UIKit
+
+class GroupCreatorTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
