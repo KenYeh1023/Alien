9e3b0cf58deab70b66ec470adbe029b573fd21c7
KenYeh1023 <s1011451@gmail.com>
2019-09-04 18:03:48 +0800

    通知頁面新增批准/拒絕按鈕

---
+++ Alien/NotificationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationTableViewCell.swift
+//  Alien
+//
+//  Created by Ken Yeh on 2019/9/3.
+//  Copyright © 2019 Ken Yeh. All rights reserved.
+//
+
+import UIKit
+
+class NotificationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
