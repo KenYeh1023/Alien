9a7db4957a63dd474ec2dcf2a77f8592e7366f6a
KenYeh1023 <s1011451@gmail.com>
2019-09-05 15:20:15 +0800

    新增TabBarController Swift File

---
+++ Alien/GroupListTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  GroupListTabBarController.swift
+//  Alien
+//
+//  Created by Ken Yeh on 2019/9/5.
+//  Copyright © 2019 Ken Yeh. All rights reserved.
+//
+
+import UIKit
+
+class GroupListTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
